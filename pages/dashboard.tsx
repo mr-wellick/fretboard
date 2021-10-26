8ff7b56fb2ec65f1bf495e34a2bebce802de159e
mr-wellick <lorenz3192@gmail.com>
2021-10-25 23:51:55 -0700

    add tailwind css

---
+++ pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Fretboard } from "../src";
+
+const Dashboard: NextPage = () => {
+  return (
+    <div>
+      <Fretboard />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/fretboard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const Fretboard: NextPage = () => {
+  return <div>fretboard</div>;
+};
+
+export default Fretboard;
