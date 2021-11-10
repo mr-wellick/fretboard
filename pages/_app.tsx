f15510750825a6746ebc814b72f564d201aa180a
mr-wellick <lorenz3192@gmail.com>
2021-11-09 20:59:47 -0800

    remove drag-n-drop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../src/app/store";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
-        <Component {...pageProps} />
-      </DndProvider>
+      <Component {...pageProps} />
     </Provider>
   );
 }
